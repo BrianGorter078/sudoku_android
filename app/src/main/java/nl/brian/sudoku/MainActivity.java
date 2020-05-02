f2a9c1cc545bdee88d262493ff32ab075e0fa886
bgorter <bgorter@anwb.nl>
2020-05-02 09:28:04 +0200

    Added Sudoku rendering logic

--- app/src/main/java/nl/brian/sudoku/MainActivity.java
+++ app/src/main/java/nl/brian/sudoku/MainActivity.java
@@ -1,12 +1,16 @@
 package nl.brian.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -42,8 +46,27 @@ public class MainActivity extends AppCompatActivity {
         final Sudoku sudoku = new Sudoku().getSudoku(this);
         List<List<Integer>> sudokuStart = sudoku.getStart();
 
-        for(int i = 0; i < sudokuStart.size(); i+=1){
-            System.out.println(sudokuStart.get(i));
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+        float dpHeight = displayMetrics.heightPixels;
+        float dpWidth = displayMetrics.widthPixels;
+
+        ConstraintLayout parentLayout = findViewById(R.id.mainConstraint);
+        int topId = adView.getId();
+        for(int i = 0; i < sudokuStart.size(); i+=1) {
+            int prevViewId = -1;
+            for (int j = 0; j < 9; j += 1) {
+                if(j==0){
+                    View prevView = CreateGridItem(this,dpWidth,String.valueOf(sudokuStart.get(i).get(j)),parentLayout,topId,-1);
+                    prevViewId = prevView.getId();
+                    continue;
+                }
+
+                View view = CreateGridItem(this,dpWidth,String.valueOf(sudokuStart.get(i).get(j)),parentLayout,topId,prevViewId);
+                prevViewId = view.getId();
+                if(j == 8){
+                    topId = view.getId();
+                }
+            }
         }
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
@@ -61,6 +84,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    private View CreateGridItem(Context ctx, float dpWidth, String input,ConstraintLayout parentLayout,int topId, int leftId){
+        ConstraintSet set = new ConstraintSet();
+
+        EditText editText = new EditText(ctx);
+        if(!input.equals("0")){
+            editText.setText(input);
+        }
+        editText.setWidth(((int)dpWidth / 9));
+        editText.setId(View.generateViewId());
+
+        parentLayout.addView(editText);
+        set.clone(parentLayout);
+        set.connect(editText.getId(),ConstraintSet.TOP,topId,ConstraintSet.BOTTOM);
+        if(leftId > -1){
+            set.connect(editText.getId(),ConstraintSet.LEFT, leftId, ConstraintSet.RIGHT);
+        }
+
+        set.applyTo(parentLayout);
+
+        return editText;
+    }
 
 }
 
